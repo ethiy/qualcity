71da57044796bcf0f831541dd5f1b1d5f228d7c7
Ethiy <oussama.ennafii@ign.fr>
2017-08-17 12:15:36 +0200

    features from laplacians

--- graph_util.py
+++ graph_util.py
@@ -5,19 +5,21 @@ import numpy as np
 
 operations = ['multiplication', 'resistance', 'addition', 'scalar_product']
 
+
 def compute_weight(graph, attribute, operation):
     if operation == 'multiplication':
         for u, v, data in graph.edges(data=True):
-            data = graph.node[u][attribute] * graph.node[v][attribute]
+            print graph.node[u], graph.node[v], data
+            data['weight'] = graph.node[u][attribute] * graph.node[v][attribute]
     elif operation == 'resistance':
         for u, v, data in graph.edges(data=True):
-            data = 1 / ((1 / graph.node[u][attribute]) * (1 / graph.node[v][attribute]))
+            data['weight'] = 1 / ((1 / graph.node[u][attribute]) * (1 / graph.node[v][attribute]))
     elif operation == 'addition':
         for u, v, data in graph.edges(data=True):
-            data = graph.node[u][attribute] + graph.node[v][attribute]
+            data['weight'] = graph.node[u][attribute] + graph.node[v][attribute]
     elif operation == 'scalar_product':
         for u, v, data in graph.edges(data=True):
-            data = np.dot(graph.node[u][attribute], graph.node[v][attribute])
+            data['weight'] = np.dot(graph.node[u][attribute], graph.node[v][attribute])
     else:
         raise AttributeError
     return graph
--- main.py
+++ main.py
@@ -4,23 +4,21 @@
 import os
 import fnmatch
 
-import networkx as nx
-import numpy as np
-import matplotlib.pyplot as plt
-
 from graph_io import read
 from graph_util import operations
 
 from laplacian_features import laplacian_eigen_vectors
+
 
 def main():
     root_path = '/home/ethiy/Data/Elancourt/Bati3D/EXPORT_1246-13704/export-3DS/dual_graphs'
 
     graphs = [read(graph_file) for graph_file in fnmatch.filter([os.path.join(root_path, file) for file in os.listdir(root_path)], '*.txt')]
 
-    print [laplacian_eigen_vectors(graph, 'area', operations[2]) for graph in graphs]
-    #nx.draw(G)
-    #plt.show()
+    lapeigs = [laplacian_eigen_vectors(graph, 'area', operations[2]) for graph in graphs]
+    size = reduce(lambda x, y: min(x, y), [lapeig.shape[1] for lapeig in lapeigs])
+    features = [lapeig[:,-2:] for lapeig in lapeigs]
+
 
 if __name__ == '__main__':
     main()
