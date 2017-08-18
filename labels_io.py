5df8e637c9c4cc5315059a6d10f78d51c6d63e02
Ethiy <oussama.ennafii@ign.fr>
2017-08-18 11:12:23 +0200

    labels without set

--- labels_io.py
+++ labels_io.py
@@ -1,22 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: <utf-8> -*-
 
-import fnmatch
-import os
-
 import shapefile
 
 SHAPEFILE = "ESRI Shapefile"
 
 
 def read(filename):
-    return set([feature[-1] for feature in shapefile.Reader(filename).records()])
-
-
-def main():
-    directory = '/home/ethiy/Data/Elancourt/Bati3D/EXPORT_1246-13704/export-3DS/labels'
-    print [read(filename) for filename in fnmatch.filter([os.path.join(directory, file) for file in os.listdir(directory)], '*.shp')]
-
-
-if __name__ == '__main__':
-    main()
+    return [feature[-1] for feature in shapefile.Reader(filename).records()]
--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 import os
 import fnmatch
 
-from graph_io import read
+import graph_io
+import labels_io
+
 from graph_util import operations
 
 from laplacian_features import laplacian_features
@@ -20,7 +22,7 @@ def graph_files(directory):
 def main():
     root_path = '/home/ethiy/Data/Elancourt/Bati3D/EXPORT_1246-13704/export-3DS/dual_graphs'
 
-    graphs = [read(graph_file) for graph_file in graph_files(root_path)]
+    graphs = [graph_io.read(graph_file) for graph_file in graph_files(root_path)]
 
     print laplacian_features(graphs, 'area', operations[2])[1]
 
