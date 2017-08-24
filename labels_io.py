331e3aaeef7adeed5ed7c8f59917a5bfd32313b8
Ethiy <oussama.ennafii@ign.fr>
2017-08-24 18:15:00 +0200

    new labels format

--- labels_io.py
+++ labels_io.py
@@ -1,10 +1,23 @@
 #! /usr/bin/env python
 # -*- coding: <utf-8> -*-
 
+import os
+import fnmatch
+
 import shapefile
 
 SHAPEFILE = "ESRI Shapefile"
 
 
 def read(filename):
-    return [feature[-1] for feature in shapefile.Reader(filename).records()]
+    return [(feature[-3], feature[-2], feature[-1]) for feature in shapefile.Reader(filename).records()]
+
+
+def main():
+    labels_dir = '/home/ethiy/Data/Elancourt/Bati3D/EXPORT_1246-13704/export-3DS/_labels'
+    files = fnmatch.filter(os.listdir(labels_dir), '*.shp')
+    print {os.path.splitext(f)[0]: read(os.path.join(labels_dir, f)) for f in files}
+
+
+if __name__ == '__main__':
+    main()
