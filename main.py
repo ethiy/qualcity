d9a6f3d2f1391829d10d361c5ffad9f3415fcc7d
Ethiy <oussama.ennafii@ign.fr>
2017-09-29 18:04:06 +0200

    compare rfs

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 import graph_io
+import utils
 import labels_io
 
 
@@ -59,6 +60,74 @@ def labels_map(directory):
             '*.shp'
         )
     }
+
+
+def random_forests_stats(depths, number_of_estimators, features, labels, cv):
+    depth_vs_nbr = [
+        [
+            skmodsel.cross_validate(
+                skens.RandomForestClassifier(
+                    n_estimators=size,
+                    class_weight="balanced",
+                    max_depth=depth,
+                    oob_score=True,
+                    n_jobs=-1
+                ),
+                features,
+                labels,
+                cv=cv
+            )['test_score']
+            for depth in depths
+        ]
+        for size in number_of_estimators
+    ]
+
+    min_depth_vs_nbr = np.array(
+        [
+            [
+                min(values)
+                for values in row
+            ]
+            for row in depth_vs_nbr
+        ]
+    )
+    max_depth_vs_nbr = np.array(
+        [
+            [
+                max(values)
+                for values in row
+            ]
+            for row in depth_vs_nbr
+        ]
+    )
+    median_depth_vs_nbr = np.array(
+        [
+            [
+                utils.median(values)
+                for values in row
+            ]
+            for row in depth_vs_nbr
+        ]
+    )
+    return (
+            min_depth_vs_nbr,
+            max_depth_vs_nbr,
+            median_depth_vs_nbr
+        )
+
+
+def plot_rf_stats(min_rf, max_rf, median_rf):
+    f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey='row')
+    min_plt = ax1.imshow(min_rf, cmap='jet')
+    ax1.set_title('minimum')
+    f.colorbar(min_plt, ax=ax1)
+    max_plt = ax2.imshow(max_rf, cmap='jet')
+    f.colorbar(max_plt, ax=ax2)
+    ax2.set_title('maximum')
+    median_plt = ax3.imshow(median_rf, cmap='jet')
+    f.colorbar(median_plt, ax=ax3)
+    ax3.set_title('median')
+    f.show()
 
 
 def vizualize_tree(idx, tree):
@@ -169,6 +238,26 @@ def main():
         cv=10
     )
     print "Time taken =", time.time() - start, 'sec'
+
+    start = time.time()
+    (min_rf, max_rf, median_rf) = random_forests_stats(
+        range(1, 11),
+        [25 * sz for sz in range(1, 41)],
+        qualified_features,
+        qualified_labels,
+        10
+    )
+    print "Time taken =", time.time() - start, 'sec'
+
+    # np.savetxt(
+    #     './ressources/output/randomforest/matrix_rf.csv',
+    #     np.dstack(
+    #         [min_rf, max_rf, median_rf]
+    #     ),
+    #     delimiter=','
+    # )
+
+    plot_rf_stats(min_rf, max_rf, median_rf)
 
     fig = plt.figure()
     ax = Axes3D(fig)
