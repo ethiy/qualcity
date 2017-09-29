c36a812df3ce26c175aa2735b0dd43e255afd61c
Ethiy <oussama.ennafii@ign.fr>
2017-09-29 18:04:22 +0200

    base for clustering classifs

---
+++ clustering_classifier.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: <utf-8> -*-
+
+import scikit-learn.base as skbase
+
+
+class ClusteringClassifier(skbase.BaseEstimator, skbase.ClassifierMixin):
+    def __init__():
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
