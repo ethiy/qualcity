b62335e441a554d7d55402615ad1497be32ec57b
Ethiy <oussama.ennafii@ign.fr>
2017-10-26 16:29:50 +0200

    to class automation

---
+++ qualcity.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+# -*- coding: <utf-8> -*-
+
+"""qualcity.
+
+Usage:
+    qualcity.py (-h | --help)
+    qualcity.py pipeline <config_file> [--verbose]
+
+Options:
+    -h --help           Show this screen.
+
+"""
+
+from docopt import docopt
+
+import yaml
+
+
+def load_config(file):
+    with open(file, mode='r') as conf:
+        return yaml.load(conf)
+
+
+def main():
+    arguments = docopt(__doc__, help=True, version=None, options_first=False)
+    if arguments['--verbose']:
+        print(yaml.dump(load_config(arguments['<config_file>'])))
+
+
+if __name__ == '__main__':
+    main()
