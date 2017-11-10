e59e52eaafc5a56d3e11063eb984090a0ec8cc43
Ethiy <oussama.ennafii@ign.fr>
2017-11-10 10:29:27 +0100

    qualcity + log

--- qualcity.py
+++ qualcity.py
@@ -12,7 +12,9 @@ Options:
 
 """
 
-from docopt import docopt
+import docopt
+
+import time
 
 import yaml
 
@@ -23,76 +25,95 @@ import geometry_io
 import labels_io
 import utils
 
+logger = logging.getLogger('qualcity')
 
-def altimetric_features(level, raster_dir, granularity):
-    if level < 1:
+
+def altimetric_features(depth, raster_dir, resolution):
+    if depth < 1:
         raise ValueError
     return altimetric_difference.histogram_features(
         raster_dir,
         altimetric_difference.DSM_DIR,
-        granularity,
-        granularity
+        resolution,
+        resolution
     )
 
 
-def features(level, feat_type, **kwargs):
+def features(depth, feat_type, **kwargs):
     return {
         'geometric': lambda kwargs: geometry_io.geometric_features(**kwargs),
-        'altimetric': lambda kwargs: altimetric_features(level, **kwargs)
+        'altimetric': lambda kwargs: altimetric_features(depth, **kwargs)
     }[feat_type](kwargs)
 
 
-def get_features(level, config):
-    logging.info('Getting features ...')
+def get_features(depth, config):
+    logger.info('Getting features ...')
     return reduce(
         utils.fuse,
         [
-            features(level, feat_type, **config[feat_type])
+            features(depth, feat_type, **config[feat_type])
             for feat_type in config.keys()
         ]
     )
 
 
-def get_labels(hierarchical, level, LoD, labels_dir):
-    logging.info('Getting Labels ...')
+def get_labels(hierarchical, depth, LoD, labels_dir):
+    logger.info('Getting Labels ...')
     return labels_io.labels_map(
         labels_dir,
         hierarchical,
-        level,
+        depth,
         LoD,
         threshold=5
     )
 
 
 def load_config(file):
-    logging.info('Loading pipeline configuration file...')
+    logger.info('Loading pipeline configuration file...')
     with open(file, mode='r') as conf:
         return yaml.load(conf)
 
 
 def main():
-    arguments = docopt(__doc__, help=True, version=None, options_first=False)
-
-    logging.basicConfig(
-        level=logging.DEBUG if arguments['--verbose'] else logging.INFO
+    arguments = docopt.docopt(
+        __doc__,
+        help=True,
+        version=None,
+        options_first=False
     )
 
+    logger.setLevel(logging.DEBUG if arguments['--verbose'] else logging.INFO)
+
+    fh = logging.FileHandler('qualcity-' + time.ctime() + '.log')
+    fh.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.WARN)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
     configuration = load_config(arguments['<config_file>'])
-    logging.debug(yaml.dump(configuration))
-    logging.info('Pipeline loaded.')
+    logger.debug(yaml.dump(configuration))
+    logger.info('Pipeline loaded.')
 
     labels = get_labels(
         **configuration['labels']
     )
-    logging.debug('Labels are: %s', labels)
-    logging.info('Labels safely loaded.')
+    logger.debug('Labels are: %s', labels)
+    logger.info('Labels safely loaded.')
 
     features = get_features(
-        configuration['labels']['level'],
+        configuration['labels']['depth'],
         configuration['features']
     )
-    logging.debug(features)
-    logging.info('Features safely loaded.')
+    logger.debug(features)
+    logger.info('Features safely loaded.')
 
 
 if __name__ == '__main__':
