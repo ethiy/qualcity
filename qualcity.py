fe3c18cfd8da08f5ff9a3d8ac689ffdc40623184
Ethiy <oussama.ennafii@ign.fr>
2018-01-17 11:37:08 +0100

    visualization pipline complete for simple case

--- qualcity.py
+++ qualcity.py
@@ -27,9 +27,14 @@ import colorlog
 
 import sklearn.decomposition
 import sklearn.manifold
+import sklearn.preprocessing
 
 import sklearn.neural_network
 
+import numpy as np
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 import altimetric_difference
 import geometry_io
@@ -121,99 +126,239 @@ def get_features(depth, config):
     )
 
 
+def format_features(depth, buildings, config):
+    features = get_features(depth, config)
+    features = [features[building] for building in buildings]
+
+    if depth == 0:
+        return [feature for feature, _ in features]
+    else:
+        return [np.hstack(feature) for feature in features]
+
+
 def get_labels(hierarchical, depth, LoD, labels_dir):
     logger.info('Getting Labels ...')
-    return labels_io.labels_map(
-        labels_dir,
-        hierarchical,
-        depth,
-        LoD,
-        threshold=5
-    )
+    return [
+        (building, label)
+        for building, label in sorted(
+            labels_io.labels_map(
+                labels_dir,
+                hierarchical,
+                depth,
+                LoD,
+                threshold=5
+            ).items(),
+            key=operator.itemgetter(0)
+        )
+    ]
 
 
-def visualize_features(features, labels, **visualization_args):
+def format_labels(hierarchical, depth, LoD, labels_dir):
+    logger.info('Formatting labels')
+    labels = get_labels(hierarchical, depth, LoD, labels_dir)
+    if depth > 0:
+        labels = [
+            (building, label)
+            for building, label in labels
+            if label != 'Unqualifiable'
+        ]
+    return zip(*labels)
+
+
+def visualize_features(features, labels, depth, hier, **visualization_args):
     logger.info('Visualizing features...')
+    if depth > 2 or hier is False:
+        logger.error('Not yet implemented')
+        raise NotImplementedError
     features = build_maniflod(
-        visualization_args['manifold']
+        **visualization_args['manifold']
     ).fit_transform(features)
+    logger.debug('New transformed features: %s', features)
+    logger.info('Fitted and transformed features.')
 
     features = dimension_reduction(
-        visualization_args['dimension_reduction']
+        **visualization_args['dimension_reduction']
     ).fit_transform(features)
+    logger.debug('New reduced features: %s', features)
+    logger.info('Fitted and reduced features.')
+
+    features_per_errors = {
+        cat: np.array(
+            [
+                features[idx]
+                for idx, _
+                in [
+                    (idx, label)
+                    for idx, label in enumerate(labels)
+                    if label == cat
+                ]
+            ]
+        )
+        for cat in set(labels)
+    }
+    logger.debug('separated features: %s', features_per_errors)
+    logger.info('Safely separated features by categories.')
+
+    if (
+        visualization_args['dimension_reduction']['parameters']
+        ['n_components'] == 2
+    ):
+        fig, ax = plt.subplots()
+    else:
+        figure = plt.figure(1)
+        ax = Axes3D(figure)
+
+    visualize_categories(
+        ax,
+        features_per_errors,
+        **visualization_args['style']
+    )
+    logger.info('Succesfully visualized categories')
+
+
+def visualize_categories(ax, features_per_errors, **style_args):
+    logger.info('Visualizing categories...')
+    number_of_categories = len(features_per_errors)
+    for color, marker, (label, features) in zip(
+        style_args['colors'][:number_of_categories],
+        style_args['markers'][:number_of_categories],
+        features_per_errors.items()
+    ):
+        visualize_category(
+            ax,
+            color,
+            marker,
+            label,
+            features
+        )
+    ax.legend()
+
+
+def visualize_category(ax, color, marker, label, features):
+    logger.info(
+        'Visualizing category: %s with color %s and %s marker',
+        label,
+        color,
+        marker
+    )
+    logger.info('Unpacking coordinates...')
+    coordinates = zip(
+        *[
+            list(couple)
+            for couple
+            in list(
+                features
+            )
+        ]
+    )
+    logger.debug('Unpacked %s coordinates: %s', label, coordinates)
+    ax.scatter(*coordinates, label=label, c=color, marker=marker)
 
 
 def build_maniflod(**manifold_args):
     logger.info('Building a manifold transformer...')
-    if manifold_args['algorithm'] is 'PCA':
+    if manifold_args['algorithm'] == 'PCA':
         return sklearn.decomposition.PCA(
             **manifold_args['parameters']
         )
-    elif manifold_args['algorithm'] is 'KernelPCA':
-        return sklearn.decomposition.KernelPCA()
-    elif manifold_args['algorithm'] is 'DictionaryLearning':
+    elif manifold_args['algorithm'] == 'KernelPCA':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
+        return sklearn.decomposition.KernelPCA(
+            **manifold_args['parameters']
+        )
+    elif manifold_args['algorithm'] == 'DictionaryLearning':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
         return sklearn.decomposition.DictionaryLearning(
             **manifold_args['parameters']
         )
-    elif manifold_args['algorithm'] is 'ICA':
+    elif manifold_args['algorithm'] == 'ICA':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
         return sklearn.decomposition.FastICA(
             **manifold_args['parameters']
         )
-    elif manifold_args['algorithm'] is 'FactorAnalysis':
-        return sklearn.decomposition.FactorAnalysis(
+    elif manifold_args['algorithm'] == 'FactorAnalys':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
+        return sklearn.decomposition.FactorAnalys(
             **manifold_args['parameters']
         )
-    elif manifold_args['algorithm'] is 'SpectralEmbedding':
+    elif manifold_args['algorithm'] == 'SpectralEmbedding':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
         return sklearn.manifold.SpectralEmbedding(
             **manifold_args['parameters']
         )
-    elif manifold_args['algorithm'] is 'LocallyLinearEmbedding':
+    elif manifold_args['algorithm'] == 'LocallyLinearEmbedding':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
         return sklearn.manifold.LocallyLinearEmbedding(
             **manifold_args['parameters']
         )
-    elif manifold_args['algorithm'] is 'Isomap':
+    elif manifold_args['algorithm'] == 'Isomap':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
         return sklearn.manifold.Isomap(
             **manifold_args['parameters']
         )
-    elif manifold_args['algorithm'] is 'MDS':
+    elif manifold_args['algorithm'] == 'MDS':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
         return sklearn.manifold.MDS(
             **manifold_args['parameters']
         )
-    elif manifold_args['algorithm'] is 'TSNE':
+    elif manifold_args['algorithm'] == 'TSNE':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
         return sklearn.manifold.TSNE(
             **manifold_args['parameters']
         )
-    elif manifold_args['algorithm'] is 'RBM':
+    elif manifold_args['algorithm'] == 'RBM':
+        logger.info('Manifold %s build', manifold_args['algorithm'])
         return sklearn.neural_network.BernoulliRBM(
             **manifold_args['parameters']
         )
     else:
-        logger.error('Manifold %s is not supported.', manifold)
-        LookupError
-    logger.info('Manifold %s build', manifold)
+        logger.error(
+            'Manifold %s is not supported.',
+            manifold_args['algorithm']
+        )
+        raise LookupError
 
 
-def dimension_reduction(reductor, **reductor_args):
+def dimension_reduction(**reductor_args):
     logger.info('Building a dimension reductor...')
-    if reductor is 'PCA':
-        return sklearn.decomposition.PCA(**reductor_args)
+
+    if reductor_args['parameters']['n_components'] > 3:
+        logger.error('Cannot visualize more than three dimensions!')
+        raise ValueError
+    elif reductor_args['parameters']['n_components'] < 2:
+        logger.error('Cannot visualize less than two dimensions!')
+        raise ValueError
+
+    if reductor_args['algorithm'] == 'PCA':
+        logger.info('dimension reductor %s build', reductor_args['algorithm'])
+        return sklearn.decomposition.PCA(**reductor_args['parameters'])
     else:
-        logger.error('Reductor %s is not supported.', manifold)
-        LookupError
+        logger.error(
+            'Reductor %s is not supported.',
+            reductor_args['algorithm']
+        )
+        raise LookupError
 
 
 def classify(features, labels, **kwargs):
     logger.info('Classifying...')
 
 
-def process(features, labels, **kwargs):
+def process(features, labels, depth, hierarchical, **kwargs):
     logger.info('Processing features...')
     if 'visualization' in kwargs.keys():
         logger.info('Feature space visualization.')
-        visualize_features(features, labels, **kwargs['visualization'])
+        visualize_features(
+            features,
+            labels,
+            depth,
+            hierarchical,
+            **kwargs['visualization']
+        )
 
     logger.info('Classification process')
     classify(features, labels, **kwargs['classification'])
+    plt.show()
 
 
 def load_pipeline_config(pip_conf):
@@ -235,20 +380,32 @@ def main():
     configuration = load_pipeline_config(arguments['<pipeline_config>'])
     logger.info('Pipeline loaded.')
 
-    labels = get_labels(
+    buildings, labels = format_labels(
         **configuration['labels']
+    )
+    logger.debug(
+        'There are %s buildings. Buildings are: %s',
+        len(buildings),
+        buildings
     )
     logger.debug('Labels are: %s', labels)
     logger.info('Labels safely loaded.')
 
-    features = get_features(
+    features = format_features(
         configuration['labels']['depth'],
+        buildings,
         configuration['features']
     )
     logger.debug(features)
     logger.info('Features safely loaded.')
 
-    process(features, labels, **configuration['processing'])
+    process(
+        features,
+        labels,
+        configuration['labels']['depth'],
+        configuration['labels']['hierarchical'],
+        **configuration['processing']
+    )
 
 
 if __name__ == '__main__':
