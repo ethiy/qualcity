323db2edc483fc2e363de452297ccff48061d041
Ethiy <oussama.ennafii@ign.fr>
2018-03-13 19:24:33 +0100

    less bugs in test

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: <utf-8> -*-
 
-import qualcity.utils
+from qualcity import utils
 
 import unittest
 
@@ -12,16 +12,16 @@ class UtilsTest(unittest.TestCase):
     """
 
     def test_median(self):
-        self.assertEqual(median([-5, -5, -3, -4, 0, -1]), -3.5)
-        self.assertEqual(median([-5, -5, -4, 0, -1]), -4.0)
+        self.assertEqual(utils.median([-5, -5, -3, -4, 0, -1]), -3.5)
+        self.assertEqual(utils.median([-5, -5, -4, 0, -1]), -4.0)
 
     def test_mean(self):
-        self.assertEqual(mean((-5, -5, -3, -4, 0, -1)), -3.0)
-        self.assertEqual(mean([-5, -5, -4, 0, -1]), -3.0)
+        self.assertEqual(utils.mean((-5, -5, -3, -4, 0, -1)), -3.0)
+        self.assertEqual(utils.mean([-5, -5, -4, 0, -1]), -3.0)
 
     def test_dict_fuse(self):
         self.assertEqual(
-            fuse(
+            utils.fuse(
                 {'4': 545, '10': 641, '8': 45},
                 {'4': 15, '10': 8574, '7': 9473}
             ),
@@ -35,24 +35,24 @@ class UtilsTest(unittest.TestCase):
 
     def test_stat_func(self):
         with self.assertRaises(KeyError):
-            stat(max)
+            utils.stat(max)
 
         with self.assertRaises(KeyError):
-            stats([12, 5, 0, 11.1], ['max', 'min', 'histogram'])
+            utils.stats([12, 5, 0, 11.1], ['max', 'min', 'histogram'])
 
         assertEqual(
-            stats([12, 5, 0, 11.1], 'histogram', bins=range(0, 20)),
+            utils.stats([12, 5, 0, 11.1], 'histogram', bins=range(0, 20)),
             [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0]
         )
 
     def test_resolve(self):
         self.assertEqual(
-            str(resolve('sklearn.decomposition.PCA')),
+            str(utils.resolve('sklearn.decomposition.PCA')),
             '<class \'sklearn.decomposition.pca.PCA\'>'
         )
 
         with self.assertRaisesRegex(ValueError, 'Could not resolve'):
-            resolve('sklearn.decomp.PCA')
+            utils.resolve('sklearn.decomp.PCA')
 
 
 def main():
